af2a173a89585338364124d2d27cf1ed9c4d1188
Junhyun Park <junhypar@student.42seoul.kr>
2021-04-09 17:56:01 +0900

    hi

--- srcs/wordpress/wordpress_data.sql
+++ srcs/wordpress/wordpress_data.sql
@@ -107,8 +107,8 @@ CREATE TABLE `wp_options` (
 --
 
 INSERT INTO `wp_options` (`option_id`, `option_name`, `option_value`, `autoload`) VALUES
-(1, 'siteurl', 'https://MINIKUBE_IP:5050', 'yes'),
-(2, 'home', 'https://MINIKUBE_IP:5050', 'yes'),
+(1, 'siteurl', 'http://MINIKUBE_IP:5050', 'yes'),
+(2, 'home', 'http://MINIKUBE_IP:5050', 'yes'),
 (3, 'blogname', 'junhypar', 'yes'),
 (4, 'blogdescription', '다른 워드프래스 사이트', 'yes'),
 (5, 'users_can_register', '0', 'yes'),
